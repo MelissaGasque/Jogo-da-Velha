c1bc7699a1be4f388132ad79ddaeba575bcbefcb
Melissa Gasque <melissagasque@hotmail.com>
2022-11-09 14:34:58 -0300

    Update jogo-velha.js

--- jogo-velha.js
+++ jogo-velha.js
@@ -14,17 +14,6 @@ function clicar(casa){
        //ATENÇÃO!! É importante adicionar o this 
 }
 
-    document.getElementById("c1").innerHTML = "" 
-    document.getElementById("c2").innerHTML = "" 
-    document.getElementById("c3").innerHTML = "" 
-    document.getElementById("c4").innerHTML = "" 
-    document.getElementById("c5").innerHTML = "" 
-    document.getElementById("c6").innerHTML = "" 
-    document.getElementById("c7").innerHTML = "" 
-    document.getElementById("c8").innerHTML = "" 
-    document.getElementById("c9").innerHTML = ""      
-
-  
 function reset(){
     document.getElementById("c1").innerHTML = "" 
     document.getElementById("c2").innerHTML = "" 
